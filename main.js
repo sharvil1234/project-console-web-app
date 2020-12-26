b512f937aa9a96a4f6b047a09ddc6734f52b355f
sharvil1234 <71960080+sharvil1234@users.noreply.github.com>
2020-12-26 16:31:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first_name = "Sharvil ";
+var last_name = "Bhor";
+var first_number = 84;
+var second_number = 42;
+
+console.log("My Console Project");
+
+console.log(first_name);
+console.log(last_name);
+console.log(first_name.concat(last_name));
+
+console.log( "84 + 42 = " + (first_number + second_number));
+console.log("84 - 42 = " + (first_number - second_number));
+console.log("84 x 42 = " + (first_number * second_number));  
+console.log("84 Divided By 42 = " + (first_number / second_number));  
